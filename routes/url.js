87ef27dc77fe4f3fab594950e7a565a5ff813176
Chir <chirpatelss@gmail.com>
2021-02-27 22:54:43 +0530

    .

--- routes/url.js
+++ routes/url.js
@@ -12,7 +12,7 @@ const Url = require('../module/url');
 router.post('/shorten',async (req,res)=>{
     const {longUrl} = req.body;
     const baseUrl = process.env.baseUrl;
-
+    console.log(baseUrl);
     if(!validUrl.isUri(baseUrl)){
         return res.status(401).json('Invalid base url');
     }
